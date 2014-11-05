4e27059316bd061333673624489bb2b7e5f45d58
XUZMA <xuzmnsccsz@yeah.net>
2014-11-05 21:11:09 +0800

    an algorithm of aoe(area of effect)

---
+++ code/aoe.erl
@@ -0,0 +1,333 @@
+%% author: xzm
+%% date: 2014-11-05
+
+
+%% ------------------------------------------------************************************************------------------------------------------------
+
+%% in game terminology, 
+%% aoe is the  abbreviation of "area of effect",
+%% and aoi is the that of "area of interest".
+%% aoe can be treated as a special case of aoi.
+%% generally speaking, aoi is a special case of collision detection in computational graphics.
+%% this module gives one algotihm to determine aoe and aoi.
+
+%% algorithmic cue:
+%%
+%% 1. treat the display screen as an infinite Cartesian plane.
+%%     (1) the display screen is a 2d infinite plane, denoted as plane_2d;
+%%     (2) establish a Cartesian coordinates system(X-Y axes) to represent the display screen;
+%%     (2) the center of display screen has coordinates (0,0);
+%%     (3) the axes are parallel to the frame borders of the physical display screen respectively.
+%%
+%% 2. strictly partition the plane into a series of cells.
+%%     (1) the plane is partitioned into square cells according to screen resolution and pixel size;
+%%     (2) roughly speaking, each cell is surrounded by four edges: 
+%%                edge_l(left-side edge), 
+%%                edge_r(right-side edge), 
+%%                edge_b(bottom edge),
+%%                edge_t(top edge);
+%%     (3) the above four edges intersect at four vertexes:
+%%                vertex_lb(cross point of edge_l and edge_b), 
+%%                vertex_lt(cross point of edge_l and edge_t), 
+%%                vertex_rb(cross point of edge_r and edge_b), 
+%%                vertex_rt(cross point of edge_r and edge_t);
+%%     (4) to guarantee a strictly mathematical partition, 
+%%                only vertex_lb, edge_l without vertex_lt, and edge_b without vertex_rb are included in the cell;
+%%                while vertex_lt, vertex_rb, vertex_rt, edge_r, edge_t are not included in the cell;
+%%     (5) point(x_lb, y_lb) can be the representive of the cell whose vertex_lb coordinates is (x_lb, y_lb);
+%%                hence, the cell is represented as cell(x_lb, y_lb).
+%%     (6) define the cell's dimension sizes as cl(the cell length in X-axis direction), cw(the cell width in Y-axis direction);
+%%     (7) Now, we get a partition of the 2d plane as follows:
+%%                plane_2d = {cell(m*cl, n*cw) | m, n are two arbitrary integer}.
+%%
+%% 3. refine cell resolution.
+%%     (1) to improve the resolution of cell, partition the above 2d infinite plane with refined cells
+%%        whose dimension sizes as rcl=cl/2, and rcw=cw/2.
+%%     (2) denote the refined cell whose vertex_lb is point(x_lb, y_lb) as rcell(x_lb, y_lb).
+%%     (3) now we have:
+%%                plane_2d = {rcell(m*rcl, n*rcw) | m, n are two arbitrary integer},
+%%     (4) partition a cell into 4 refined cells. for arbitrary integers m, n, 
+%%                cell(m*cl, n*cw) = cell_lb(m*cl, n*cw) +cell_lt(m*cl, n*cw) +cell_rb(m*cl, n*cw) +cell_rt(m*cl, n*cw) ,
+%%        where
+%%                cell_lb(m*cl, n*cw)  = rcell((2m)*rcl, (2n)*rcw) ,
+%%                cell_lt(m*cl, n*cw)   = rcell((2m)*rcl, (2n + 1)*rcw) ,
+%%                cell_rb(m*cl, n*cw)  = rcell((2m+1)*rcl, (2n)*rcw) ,
+%%                cell_rt(m*cl, n*cw)   = rcell((2m+1)*rcl, (2n+1)*rcw) .
+%%     (5) furtherly, define
+%%                rrcl=rcl/2, and rrcw=rcw/2;
+%%                plane_2d = {rrcell(m*rrcl, n*rrcw) | m, n are two arbitrary integer},
+%%                and for any arbitrary integers m,n, 
+%%                rcell(m*rcl, n*rcw) = rcell_lb(m*rcl, n*rcw) +rcell_lt(m*rcl, n*rcw) +rcell_rb(m*rcl, n*rcw) +rcell_rt(m*rcl, n*rcw) ,
+%%                where
+%%                    rcell_lb(m*rcl, n*rcw) = rrcell(2m*rrcl, 2n*rrcw) ,
+%%                    rcell_lt(m*rcl, n*rcw)  = rrcell(2m*rrcl, (2n+1)*rrcw) ,
+%%                    rcell_rb(m*rcl, n*rcw) = rrcell((2m+1)*rrcl, 2n*rrcw) ,
+%%                    rcell_rt(m*rcl, n*rcw)  = rrcell((2m+1)*rrcl, (2n+1)*rrcw) .
+%%     (6) the refinement can proceed to pixel level.
+%%
+%% 4. define the center of cell and refined cell.
+%%     (1) for cell(x_lb, y_lb), define
+%%                center(cell(x_lb, y_lb)) = point(x_lb + rcl, y_lb + rcw);
+%%     (2) for rcell(x_lb, y_lb), define
+%%                center(rcell(x_lb, y_lb)) = point(x_lb + rcl/2, y_lb + rcw/2);
+%%     (3) for rrcell(x_lb, y_lb), define
+%%                center(rrcell(x_lb, y_lb)) = point(x_lb + rrcl/2, y_lb + rrcw/2).
+%%
+%% 5. define the affiliated cell, vertexes, centers for a plane point,  i.e. the player's position.
+%%     (1) suppose the player is place at point(xp, yp), denoted as pos(player) = point(xp,yp);
+%%     (2) calculate the affiliated vertexes gradually as follows:
+%%                 (2.1)    let x = floor(xp/cl)*cl,     y = floor(yp/cw)*cw
+%%                                NOTICE: beware of the cases when xp==floor(xp/cl)*cl, or yp == floor(yp/cw)*cw.
+%%                 (2.2)    dimension: (cl, cw),
+%%                                cell vertexes:  
+%%                                    {(x,y )}
+%%                                center:        (x+rcl, y+rcw)
+%%                 (2.3)    dimension: (rcl, rcw),
+%%                                cell vertexes: 
+%%                                   {(x,y ),    (x+rcl, y), 
+%%                                    (x, y+rcw),    (x+rcl, y+rcw)}
+%%                 (2.4)    dimension: (rrcl,rrcw),
+%%                                cell vertexes: 
+%%                                   {(x, y), (x+rrcl, y), (x+rcl, y), (x+3*rrcl, y);
+%%                                    (x, y+rrcw), (x+rrcl, y+rrcw), (x+rcl, y+rrcw), (x+3*rrcl, y+rrcw);
+%%                                    (x, y+rcw), (x+rrcl, y+rcw), (x+rcl, y+rcw), (x+3*rrcl, y+rcw);
+%%                                    (x, y+3*rrcw), (x+rrcl, y+3*rrcw), (x+rcl, y+3*rrcw), (x+3*rrcl, y+3*rrcw)}.
+%%     (3) obviously, further refinement from level n to level n+1,
+%%                cell vertexes at level n+1 =
+%%                        cell vertexes at level n                                    +
+%%                        centers of cells at level n                                +
+%%                        center of horizontal side of cells at level n     +
+%%                        center of vertical side of cells at level n.
+%%
+%% 6. refinement is down-sampling(shrink).  neighboring is up-sampling(expansion).
+%%     compliant with the notations in 5.(1) and 5.(2.1),
+%%     player in cell(x,y),
+%%     the neighboring cells constitue of the following 9 cells, including cell(x, y) itself:
+%%
+%%                neighbor_region(x,y)
+%%                        =
+%%                                cell(x-cl,y+cw)     + cell(x,y+cw)      + cell(x+cl,y+cw)       +
+%%                                cell(x-cl,y)           + cell(x,y)            + cell(x+cl,y)              +
+%%                                cell(x-cl,y-cw)      + cell(x,y-cw)      + cell(x+cl,y-cw).
+%%
+%% 7. define the attack field.
+%%    when a player or the antagonist release an attack, the attack takes effect only in a special field,
+%%    which is a subset of the 2d plane.
+%%    we call it the attack field, and denote it as attack_field.
+%%    different points in attack_field may have different effect tension.
+%%    for example,
+%%                just as shown in Newton's law of universal gravitation,
+%%                F = G * m_1 * m_2 / r^2.
+%%
+%% 8. coefficient between attack_field and neighbor_region(x,y).
+%%     (1)  Expansion: get neighbor_region(x,y);
+%%     (2)  Refinement: get the 9*16(=12^2) cell vertexes in neighbor_region(x,y) to dimenison(rrcl, rrcw);
+%%     (3)  calculate the coefficient between attack_field and neighbor_region(x,y),
+%%                coeff = kronecker_delta(P, attack_field)  * weight(P),
+%%                where 
+%%                    kronecker_delta(P, attack_field)  takes 1 if P is an element of attack_field, else 0;
+%%                    weight(P) can be set by various policy.
+%%
+%% 9. a policy for calculating weight(P).
+%%
+%%    positions of the 9*16(=12^2) cell vertexes arelisted as follows:
+%%
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%                .    .    .    .    .    .    .    .    .    .    .    .
+%%
+%%     use a policy(called as  cicle policy) to group the vertexes, illustrated in the following figure:
+%%
+%%                6   6   6    6    6    6    6    6    6    6    6    6
+%%                6   5   5    5    5    5    5    5    5    5    5    6
+%%                6   5   4    4    4    4    4    4    4    4    5    6
+%%                6   5   4    3    3    3    3    3    3    4    5    6
+%%                6   5   4    3    2    2    2    2    3    4    5    6
+%%                6   5   4    3    2    1    1    2    3    4    5    6
+%%                6   5   4    3    2    1    1    2    3    4    5    6
+%%                6   5   4    3    2    2    2    2    3    4    5    6
+%%                6   5   4    3    3    3    3    3    3    4    5    6
+%%                6   5   4    4    4    4    4    4    4    4    5    6
+%%                6   5   5    5    5    5    5    5    5    5    5    6
+%%                6   6   6    6    6    6    6    6    6    6    6    6
+%%
+%%    in circle 1, there are 4*1 vertexes, the side length is 2;
+%%    in circle 2, there are 2*4+4 = 4*3 vertexes, the side length is 2+2=4;
+%%    in circle 3, there are 4*4+4 = 4*5 vertexes, the side length is 4+2=6;
+%%    in circle 4, there are 6*4+4 = 4*7 vertexes, the side length is 6+2=8;
+%%    in circle 5, there are 8*4+4 = 4*9 vertexes, the side length is 8+2=10;
+%%    in circle 6, there are 10*4+4 = 4*11 vertexes, the side length is 10+2=12.
+%%
+%%    let
+%%                weight(P) = 1/(4*11*6)    = wt_6, if P is a vertex in circle 6;
+%%                weight(P) = 1/(4*9*6)      = wt_5, if P is a vertex in circle 5;
+%%                weight(P) = 1/(4*7*6)      = wt_4, if P is a vertex in circle 4;
+%%                weight(P) = 1/(4*5*6)      = wt_3, if P is a vertex in circle 3;
+%%                weight(P) = 1/(4*3*6)      = wt_2, if P is a vertex in circle 2;
+%%                weight(P) = 1/(4*1*6)      = wt_1, if P is a vertex in circle 1.
+%%    i.e.
+%%                                               wt_k  = 1/24  * 1/(2*k - 1),     for k = 1,2,3,4,5,6.
+%%
+%% 10. define the strategies for determing position collision index and influence effect index.
+%%        using the above ring weight policy, in the vertex matrix, calculate weight sum falling in the diagonal trigonums gradually.
+%%
+%%                6
+%%                6   5
+%%                6   5   4
+%%                6   5   4    3
+%%                6   5   4    3    2
+%%                6   5   4    3    2    1
+%%                6   5   4    3    2    1    1
+%%                6   5   4    3    2    2    2    2
+%%                6   5   4    3    3    3    3    3    3
+%%                6   5   4    4    4    4    4    4    4    4
+%%                6   5   5    5    5    5    5    5    5    5    5
+%%                6   6   6    6    6    6    6    6    6    6    6    6
+%%                |                                                        |    |
+%%                |<-11th diagonal trigonum: trig11 ->|     the 12th diagonal line: diag12
+%%                |                                                        the 11th diagonal line: diag11
+%%                |
+%%                |
+%%                the 1st diagonal line: diag1
+%%
+%%                                               wt_k  = 1/24  * 1/(2*k - 1),     for k = 1,2,3,4,5,6.
+%%        wtd_01 = weight(diag01) = wt_6;
+%%        wtd_02 = weight(diag02) = wtd_01 + wt_6;
+%%        wtd_03 = weight(diag03) = wtd_02 + wt_5;
+%%        wtd_04 = weight(diag04) = wtd_03 + wt_5;
+%%        wtd_05 = weight(diag05) = wtd_04 + wt_4;
+%%        wtd_06 = weight(diag06) = wtd_05 + wt_4;
+%%        wtd_07 = weight(diag07) = wtd_06 + wt_3;
+%%        wtd_08 = weight(diag08) = wtd_07 + wt_3;
+%%        wtd_09 = weight(diag09) = wtd_08 + wt_2;
+%%        wtd_10 = weight(diag10)  =wtd_09 + wt_2;
+%%        wtd_11 = weight(diag11) = wtd_10 + wt_1;
+%%        wtd_12 = weight(diag12) = wtd_11 + wt_1;
+%%
+%%
+%%        wtt_01 = wtd_01;
+%%        wtt_02 = wtt_01 + wtd_02;
+%%        wtt_03 = wtt_02 + wtd_03;
+%%        wtt_04 = wtt_03 + wtd_04;
+%%        wtt_05 = wtt_04 + wtd_05;
+%%        wtt_06 = wtt_05 + wtd_06;
+%%        wtt_07 = wtt_06 + wtd_07;
+%%        wtt_08 = wtt_07 + wtd_08;
+%%        wtt_09 = wtt_08 + wtd_09;
+%%        wtt_10 = wtt_09 + wtd_10;
+%%        wtt_11 = wtt_10 + wtd_11;
+%%        wtt_12 = wtt_11 + wtd_12;
+%%
+%%    gohere.
+%%
+%% 11. calculating kronecker_delta(P, attack_field) .
+%%     use polar coordinates to represent the topological relation between two positions;
+%%     use polar coordinates to represent a player's aoe.
+%%
+%%
+%%
+%% ------------------------------------------------************************************************------------------------------------------------
+
+%% erlang implementation:
+%% 1. use modules(wx,...) to display the effect.
+
+%% ------------------------------------------------************************************************------------------------------------------------
+
+-module(aoe).
+
+-export([days_of_month/2, start/0]).
+
+%% ====================MAIN PART====================
+
+
+days_of_month(Year, Month) -> 
+%% ------------------------------------------------
+%% All years divisible by 400 are leap
+%% Years divisible by 100 are not leap (except the 400 rule above)
+%% Years divisible by 4 are leap (except the 100 rule above)
+    Leap =
+        if
+            trunc(Year / 400) * 400 == Year ->
+                leap;
+            trunc(Year / 100) * 100 == Year ->
+                not_leap;
+            trunc(Year / 4) * 4 == Year ->
+                leap;
+            true ->
+                not_leap
+        end,
+%% ------------------------------------------------
+    case Month of
+        sep -> 30;
+        apr -> 30;
+        jun -> 30;
+        nov -> 30;
+        feb when Leap == leap -> 29;
+        feb -> 28;
+        jan -> 31;
+        mar -> 31;
+        may -> 31;
+        jul -> 31;
+        aug -> 31;
+        oct -> 31;
+        dec -> 31
+    end.
+
+%% ====================TEST PART==================
+
+monthStr(Month) ->
+    case Month of
+        jan -> "Jan.";
+        feb -> "Feb.";
+        mar -> "Mar.";
+        apr -> "Apr.";
+        may -> "May.";
+        jun -> "Jun.";
+        jul -> "Jul.";
+        aug -> "Aug.";
+        sep -> "Sep.";
+        oct -> "Oct.";
+        nov -> "Nov.";
+        dec -> "Dec."
+    end.
+
+test_month_print(_,[]) ->
+    ok;
+test_month_print(Year,[MH|MT]) ->
+    io:format("There are ~p days in ~s ~p.\n",[days_of_month(Year, MH), monthStr(MH),Year]),
+    test_month_print(Year,MT).
+%%    If the last statement is written as follows:
+%%        test_month_print(Year,[MT]).
+%%    an exception error will throw:
+%%    ** exception error: no case clause matching [feb,mar,apr,may,jun,jul,aug,sep,
+%%                                                 oct,nov,dec]
+%%    Bear in mind:
+%%        [MH|MT] produce a term MH and a list MT.
+
+test_print([],_) ->
+    ok;
+test_print(_,[]) ->
+    ok;
+test_print([YH|YT],MonthList) ->
+    test_month_print(YH,MonthList),
+    test_print(YT,MonthList).
+
+test() ->
+    MonthList = [jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec],
+    YearList = [2000,2004,2008,2012,2013,2014],
+    test_print(YearList, MonthList),
+    ok.
+
+start() ->
+    test().
+
+%% ========================END=======================
