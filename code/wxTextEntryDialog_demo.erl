6e72c26bf5a2049e773ce9e5f111891c7f27f10e
XUZMA <xuzmnsccsz@yeah.net>
2014-11-10 17:19:59 +0800

    demonstrates the usage of  wxTextEntryDialog through  a Dialog to to get user name.

---
+++ code/wxTextEntryDialog_demo.erl
@@ -0,0 +1,35 @@
+%% author: xzm
+%% date: 2014-11-10
+
+%% the module demonstrates the usage of  wxTextEntryDialog through  a Dialog to get user name.
+
+-module( wxTextEntryDialog_demo).
+-export([start/0]).
+%%    ================================================
+
+%%-include_lib(“wx/include/wx.hrl”).
+-include("/usr/local/lib/erlang/lib/wx-1.3.1/include/wx.hrl").
+
+usernameDialog() ->
+    Prompt = "please entry your username!",
+    CaptionInfo = "login chat system",
+
+    wx:new(),
+    Dialog = wxTextEntryDialog:new(wx:null(),Prompt,[{caption,CaptionInfo}]),
+    case wxTextEntryDialog:showModal(Dialog) of
+        ?wxID_OK ->
+            Str = wxTextEntryDialog:getValue(Dialog);
+        ?wxID_CANCEL ->
+            Str = null
+    end,
+    wxTextEntryDialog:destroy(Dialog),
+    Str.
+
+%%    ================================================
+
+start() ->
+    Str = usernameDialog(),
+    io:format("~p~n", [Str]),
+    ok.
+
+%%    ================================================
