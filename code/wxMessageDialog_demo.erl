bbd5c336d3d9d3dc586c9510af87ef4777a1ecd3
XUZMA <xuzmnsccsz@yeah.net>
2014-11-11 11:08:30 +0800

    demo of the usage of wxMessageDialog through a departure Dialog

--- code/wxMessageDialog_demo.erl
+++ code/wxMessageDialog_demo.erl
@@ -1,18 +1,18 @@
 %% author: xzm
 %% date: 2014-11-10
 
-%% the module demonstrates the usage of  wxMessageDialog through  a leaving Dialog.
+%% the module demonstrates the usage of  wxMessageDialog through  a departure Dialog.
 
 -module( wxMessageDialog_demo).
 -export([start/0]).
 %%    ================================================
 
-%%-include_lib(“wx/include/wx.hrl”).
+%% in Eshell, run code:lib_dir(wx) to get wx lib dir: "/usr/local/lib/erlang/lib/wx-1.3.1"
 -include("/usr/local/lib/erlang/lib/wx-1.3.1/include/wx.hrl").
 
 byeDialog() ->
     Prompt = "Bye!",
-    CaptionInfo = "logout system",
+    CaptionInfo = "logout system...",
 
     wx:new(),
     Dialog = wxMessageDialog:new(wx:null(),Prompt,[{caption,CaptionInfo},{style,?wxOK bor ?wxICON_INFORMATION}]),
