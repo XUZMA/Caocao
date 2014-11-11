75d89050a6d7c850650690576c8c61c68735d806
XUZMA <xuzmnsccsz@yeah.net>
2014-11-11 11:00:12 +0800

    demo of  the usage of wxTextEntryDialog through a user name dialog

--- code/wxTextEntryDialog_demo.erl
+++ code/wxTextEntryDialog_demo.erl
@@ -1,5 +1,5 @@
 %% author: xzm
-%% date: 2014-11-10
+%% date: 2014-11-10/11-11
 
 %% the module demonstrates the usage of  wxTextEntryDialog through  a Dialog to get user name.
 
@@ -7,12 +7,12 @@
 -export([start/0]).
 %%    ================================================
 
-%%-include_lib(“wx/include/wx.hrl”).
+%% in Eshell, run code:lib_dir(wx) to get wx lib dir: "/usr/local/lib/erlang/lib/wx-1.3.1"
 -include("/usr/local/lib/erlang/lib/wx-1.3.1/include/wx.hrl").
 
 usernameDialog() ->
     Prompt = "please entry your account name:",
-    CaptionInfo = "login system",
+    CaptionInfo = "login system...",
 
     wx:new(),
     Dialog = wxTextEntryDialog:new(wx:null(),Prompt,[{caption,CaptionInfo}]),
