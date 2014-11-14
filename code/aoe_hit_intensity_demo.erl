13b260bfe99753eb1a77aea42b957ea01ecfc97f
XUZMA <xuzmnsccsz@yeah.net>
2014-11-14 17:07:52 +0800

    demo of aoe hit intensity

--- code/aoe_hit_intensity_demo.erl
+++ code/aoe_hit_intensity_demo.erl
@@ -1,7 +1,12 @@
 %% author: xzm
 %% date: 2014-11-13
 
-%% the module draw test figures for aoe.erl
+%% in aoe.erl, 
+%% the hit intensity list is 
+%% [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+%%  7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+%%  7,7,7,7,7]
+%% the module draw test figures.
 
 -module( aoe_hit_intensity_demo).
 
@@ -13,20 +18,19 @@
 %% in Eshell, run code:lib_dir(wx) to get wx lib dir: "/usr/local/lib/erlang/lib/wx-1.3.1"
 -include("/usr/local/lib/erlang/lib/wx-1.3.1/include/wx.hrl").
 
--define(cell_unit, 40).
--define(square_side, 40).
--define(center_x, 800).
--define(center_y, 400).
+-define(square_side, 10).
 
 draw_axes(Dc) ->
-    wxDC:drawLine(Dc,{800,0}, {800,820}),
-    wxDC:drawLine(Dc,{0,400}, {2000,400}).
+    wxDC:drawLine(Dc,{0,0}, {0,820}),
+    wxDC:drawLine(Dc,{0,0}, {2000,0}).
 
-draw_circle(Dc) ->
-    wxDC:drawCircle(Dc,{?center_x,?center_y},?cell_unit*4).
+draw_arc(Dc) ->
+    wxDC:drawArc(Dc,{0,200},{200,0},{0,0}).
 
-draw_square(Dc,K)->
-    wxDC:drawRectangle(Dc, {?center_x+?square_side*K,?center_y+?square_side*K}, {?square_side,?square_side}).
+draw_squares(Dc)->
+    lists:map(
+        fun({X,Y})-> wxDC:drawRectangle(Dc, {X,Y}, {?square_side,?square_side}) end,
+        lists:map(fun(X)->{X*?square_side,X*?square_side} end,lists:seq(0,80,1))).
 
 canvas_mini() ->
 
@@ -36,18 +40,19 @@ canvas_mini() ->
      wxFrame:show(Frm),
     Dc = wxClientDC:new(Frm),
     Pen = wxPen:new(),
+
+%%    ---- draw axes, squares, and circle
     wxPen:setColour(Pen, 255,0,0),
     wxDC:setPen(Dc, Pen),
-
-%%    ---- draw axes and circle
-    draw_circle(Dc),
     draw_axes(Dc),
 
-%%    ---- draw squares
+    wxPen:setColour(Pen, 0,255,0),
+    wxDC:setPen(Dc, Pen),
+    draw_arc(Dc),
+
     wxPen:setColour(Pen, 0,0,255),
     wxDC:setPen(Dc, Pen),
-    List_pt = lists:seq(-6,6,1),
-    lists:map(fun(K)->draw_square(Dc,K) end, List_pt),
+    draw_squares(Dc),
 
 %%    wxFrame:destroy(Frm).
 
