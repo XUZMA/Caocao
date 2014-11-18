db4c64a2c74c1b98ebe1cf75ae5d6c3ed1206863
XUZMA <xuzmnsccsz@yeah.net>
2014-11-18 21:01:01 +0800

    test of aoe_grid.erl

--- code/aoe_grid_test_pic.erl
+++ code/aoe_grid_test_pic.erl
@@ -2,6 +2,7 @@
 %% date: 2014-11-17
 
 %% the module draw test figures for aoe_grid.erl
+%% the bug on the output picture is caused by float precision.
 
 -module(aoe_grid_test_pic).
 
@@ -27,21 +28,57 @@ draw_axes(Dc) ->
 draw_circle(Dc) ->
     wxDC:drawCircle(Dc,{?center_x,?center_y},?radius).
 
-draw_arc(Dc) ->
-    wxDC:drawArc(Dc, 
-        {?center_x+erlang:trunc(?radius*math:cos(math:pi()/3)),?center_y+erlang:trunc(?radius*math:sin(math:pi()/3))},
-        {?center_x+erlang:trunc(?radius*math:cos(math:pi()/6)),?center_y+erlang:trunc(?radius*math:sin(math:pi()/6))},
-        {?center_x,?center_y}).
+%% draw_arc(Dc) ->
+%%     wxDC:drawArc(Dc, 
+%%         {?center_x+erlang:trunc(?radius*math:cos(math:pi()/3)),?center_y+erlang:trunc(?radius*math:sin(math:pi()/3))},
+%%         {?center_x+erlang:trunc(?radius*math:cos(math:pi()/6)),?center_y+erlang:trunc(?radius*math:sin(math:pi()/6))},
+%%         {?center_x,?center_y}).
 
 draw_lines(Dc) ->
+
+    Angle_s = math:pi()/6,
+    Angle_e = math:pi()/3,
+    Half_pi = math:pi()/2,
+
+    %% draw lines in quadrant I
     wxDC:drawLine(
         Dc,
         {?center_x,?center_y},
-        {erlang:trunc(?center_x+?radius*math:cos(math:pi()/6)), erlang:trunc(?center_y+?radius*math:sin(math:pi()/6))}),
+        {erlang:trunc(?center_x+?radius*math:cos(Angle_s)), erlang:trunc(?center_y+?radius*math:sin(Angle_s))}),
     wxDC:drawLine(
         Dc,
         {?center_x,?center_y},
-        {erlang:trunc(?center_x+?radius*math:cos(math:pi()/3)), erlang:trunc(?center_y+?radius*math:sin(math:pi()/3))}).
+      {erlang:trunc(?center_x+?radius*math:cos(Angle_e)), erlang:trunc(?center_y+?radius*math:sin(Angle_e))}),
+
+    %% draw lines in quadrant II
+    wxDC:drawLine(
+        Dc,
+        {?center_x,?center_y},
+        {erlang:trunc(?center_x+?radius*math:cos(Angle_s+Half_pi)), erlang:trunc(?center_y+?radius*math:sin(Angle_s+Half_pi))}),
+    wxDC:drawLine(
+        Dc,
+        {?center_x,?center_y},
+      {erlang:trunc(?center_x+?radius*math:cos(Angle_e+Half_pi)), erlang:trunc(?center_y+?radius*math:sin(Angle_e+Half_pi))}),
+
+    %% draw lines in quadrant III
+    wxDC:drawLine(
+        Dc,
+        {?center_x,?center_y},
+        {erlang:trunc(?center_x+?radius*math:cos(Angle_s+2*Half_pi)), erlang:trunc(?center_y+?radius*math:sin(Angle_s+2*Half_pi))}),
+    wxDC:drawLine(
+        Dc,
+        {?center_x,?center_y},
+      {erlang:trunc(?center_x+?radius*math:cos(Angle_e+2*Half_pi)), erlang:trunc(?center_y+?radius*math:sin(Angle_e+2*Half_pi))}),
+
+    %% draw lines in quadrant IV
+    wxDC:drawLine(
+        Dc,
+        {?center_x,?center_y},
+        {erlang:trunc(?center_x+?radius*math:cos(Angle_s+3*Half_pi)), erlang:trunc(?center_y+?radius*math:sin(Angle_s+3*Half_pi))}),
+    wxDC:drawLine(
+        Dc,
+        {?center_x,?center_y},
+      {erlang:trunc(?center_x+?radius*math:cos(Angle_e+3*Half_pi)), erlang:trunc(?center_y+?radius*math:sin(Angle_e+3*Half_pi))}).
 
 draw_squares_vertical(Dc,{Ox,Oy1,Oy2})->
     lists:map(
@@ -74,17 +111,23 @@ canvas_mini() ->
 %%    ---- draw squares
     wxPen:setColour(Pen, 0,0,255),
     wxDC:setPen(Dc, Pen),
-    Tuplelist_LB = [{0,0,1},{1,0,3},{2,1,5},{3,1,6},{4,2,8},{5,2,8},{6,3,8},{7,4,7},{8,4,6}],
-%% [{0,0,1},{1,0,3},{2,1,5},{3,1,6},{4,2,8},{5,2,10},{6,3,8},{7,4,7},{8,4,6}],
-%% [{0,0,0},{1,0,1},{2,1,3},{3,1,5},{4,2,6},{5,2,8},{6,3,8},{7,4,7},{8,4,6}],
 
-    draw_squares(Dc,Tuplelist_LB),
+    Tuplelist_LB_Quadrant1 = [{0,0,1},{1,0,3},{2,1,5},{3,1,6},{4,2,8},{5,2,8},{6,3,8},{7,4,7},{8,4,6}],
+    draw_squares(Dc,Tuplelist_LB_Quadrant1),
+
+    Tuplelist_LB_Quadrant2 = [{-1,0,1},{-2,0,3},{-3,1,5},{-4,1,6},{-5,2,9},{-6,2,8},{-7,3,8},{-8,4,7},{-9,4,6}],
+    draw_squares(Dc,Tuplelist_LB_Quadrant2),
+
+    Tuplelist_LB_Quadrant3 = [{-1,-2,-1},{-2,-4,-1},{-3,-6,-2},{-4,-7,-2},{-5,-9,-3},{-6,-9,-3},{-7,-9,-4},{-8,-8,-5},{-9,-7,-5}],    
+    draw_squares(Dc,Tuplelist_LB_Quadrant3),
+
+    Tuplelist_LB_Quadrant4 = [{0,-2,-1},{1,-4,-1},{2,-6,-2},{3,-7,-2},{4,-10,-3},{5,-9,-3},{6,-9,-4},{7,-8,-5},{8,-7,-5}],
+    draw_squares(Dc,Tuplelist_LB_Quadrant4),
 
 %%    ---- draw the border of the sector
     wxPen:setColour(Pen, 255,0,0),
     wxDC:setPen(Dc, Pen),
     draw_lines(Dc),
-    draw_arc(Dc),
 
 %%    wxFrame:destroy(Frm).
 
