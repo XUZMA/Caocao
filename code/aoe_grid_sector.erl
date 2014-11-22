6fb93aa15a4703c991cf4d6a3847b518550529ce
XUZMA <xuzmnsccsz@yeah.net>
2014-11-22 16:34:08 +0800

    design and implement the aoe grid algorithm for any sector region through distinct quadrants

--- code/aoe_grid_sector.erl
+++ code/aoe_grid_sector.erl
@@ -1,5 +1,5 @@
 %% author: xzm
-%% date: 2014-11-19/20/21
+%% date: 2014-11-19/20/21/22
 
 %% Algorithm description:
 %% assume the attack region is a sector, and every cell is a square;
@@ -13,9 +13,9 @@
 -export([start/0]).
 
 %%    ================ predefined macros ================
-%% ?cell_side is the length of cell side, in pixel.
+%% ?cell_side_size is the length of cell side, in pixel.
 %%        the user should modify the macro according to the actual situation.
--define(cell_side, 40).
+-define(cell_side_size, 40).
 
 -define(half_pi, (math:pi()/2)).
 -define(pi, (math:pi())).
@@ -51,17 +51,17 @@ angle_normal(Angle)->
 angle_quadrant(Angle_n)->
     erlang:trunc(2 * Angle_n / ?pi).
 
-%% Q = 0,1,2,3 is the quadrant ordinal of Angle_n_s.
+%% Q_s = 0,1,2,3 is the quadrant ordinal of Angle_n_s.
 %%     Angle_amplitude =< ?two_pi.
 %%     Angle_e = Angle_n_s + Angle_amplitude,
 %%    notice: Angle_e may not be normal.
-sector_quadrant({Q,Angle_n_s,Angle_e,Radius})->
+sector_quadrant({Q_s,Angle_n_s,Angle_e,Radius})->
     Half_pi = ?half_pi,
     Pi = ?pi,
     Half_3pi = ?half_3pi,
     Two_pi = ?two_pi,
 
-    case Q of
+    case Q_s of
         0 ->
             if
                 Angle_e =< Half_pi -> 
@@ -100,20 +100,40 @@ sector_quadrant({Q,Angle_n_s,Angle_e,Radius})->
             end
     end.
 
+
+grid_point({P_x,P_y})->
+    [G_x, G_y] = 
+        lists:map(
+            fun(P) ->
+                O = P /?cell_side_size,
+                T = erlang:trunc(O),
+                Q =
+                    if
+                    O < T -> T - 1;
+		    true -> T
+                    end
+                Q * ?cell_side_size
+            end,
+        [P_x, P_y]),
+    {G_x,G_y}.
+
+grid_point(P_x,P_y)->
+    grid_point({P_x,P_y}).
+
 %%    ================ cell (representative) points around rays and arcs ================
 yp_zenith(Radius) ->
-    erlang:trunc(Radius / ?cell_side) .
+    erlang:trunc(Radius / ?cell_side_size) .
 
 yn_zenith(Radius) ->
-    floor_neg(-Radius / ?cell_side) .
+    floor_neg(-Radius / ?cell_side_size) .
 
 %% Angle_n /= math:pi()/2 or 3*math:pi()/2.
 yp_endpoint(Angle_n,Radius) ->
-    erlang:trunc(Radius * math:tan(Angle_n) / ?cell_side) .
+    erlang:trunc(Radius * math:tan(Angle_n) / ?cell_side_size) .
 
 %% Angle_n /= math:pi()/2 or 3*math:pi()/2.
 yn_endpoint(Angle_n,Radius) ->
-    floor_neg(Radius * math:tan(Angle_n) / ?cell_side).
+    floor_neg(Radius * math:tan(Angle_n) / ?cell_side_size).
 
 %% Angle_n /= math:pi()/2 or 3*math:pi()/2.
 %% Ox /= 0.
@@ -126,17 +146,17 @@ yn_steppoint_on_ray(Ox, Angle_n) ->
     floor_neg(Ox * math:tan(Angle_n)).
 
 yp_steppoint_on_arc(Ox, Radius)->
-    erlang:trunc(math:sqrt(Radius*Radius - Ox*Ox*?cell_side*?cell_side)/?cell_side) .
+    erlang:trunc(math:sqrt(Radius*Radius - Ox*Ox*?cell_side_size*?cell_side_size)/?cell_side_size) .
 
 yn_steppoint_on_arc(Ox, Radius)->
-    floor_neg(-math:sqrt(Radius*Radius - Ox*Ox*?cell_side*?cell_side)/?cell_side).
+    floor_neg(-math:sqrt(Radius*Radius - Ox*Ox*?cell_side_size*?cell_side_size)/?cell_side_size).
 
 %% Q = 0,1,2,3 is the quadrant ordinal of Angle_n,i.e.,
 %%    Q = angle_quadrant(Angle_n);
 %% X_n is the X coordinate for the endpoint, i.e.,
 %%    X_n = Radius * math:cos(Angle_n);
-%% Dx_n is the Division of X_n by ?cell_side, i.e.,
-%%    Dx_n = X_n/?cell_side;
+%% Dx_n is the Division of X_n by ?cell_side_size, i.e.,
+%%    Dx_n = X_n/?cell_side_size;
 %% Tx_n is the Trunc of Dx_n, i.e.,
 %%    Tx_n = erlang:trunc(Dx_n);
 stepboxes_around_ray(Q, Tx_n, Dx_n, X_n, Angle_n, Radius)->
@@ -188,8 +208,6 @@ stepboxes_around_ray(Q, Tx_n, Dx_n, X_n, Angle_n, Radius)->
                     lists:zipwith(fun({Ox,Yp1},{_,Yp2})->{Ox,Yp1,Yp2}end,L2,L1)
             end;
         true ->
-%% ??? it's taken for granted to taken some outer x into the range.
-%% CHECK!!!
             case Q of
                 0 ->
                     Lst1 = [{0,0}]++lists:map(fun(Ox) -> {Ox,yp_steppoint_on_ray(Ox, Angle_n)} end,lists:seq(1,Tx_n,1)),
@@ -216,9 +234,9 @@ stepboxes_around_ray(Q, Tx_n, Dx_n, X_n, Angle_n, Radius)->
 %% X_s, X_e are the X coordinates for the endpoint, i.e.,
 %%    X_s = Radius * math:cos(Angle_n_s),
 %%    X_e = Radius * math:cos(Angle_n_e);
-%% Dx_s, Dx_e are the Divisions of X_s, X_e by ?cell_side, i.e.,
-%%    Dx_s = X_s/?cell_side,
-%%    Dx_e = X_e/?cell_side;
+%% Dx_s, Dx_e are the Divisions of X_s, X_e by ?cell_side_size, i.e.,
+%%    Dx_s = X_s/?cell_side_size,
+%%    Dx_e = X_e/?cell_side_size;
 %% Tx_s, Tx_e are the Truncs of Dx_s, Dx_e, i.e.,
 %%    Tx_s = erlang:trunc(Dx_s),
 %%    Tx_e = erlang:trunc(Dx_e);
@@ -234,173 +252,197 @@ stepboxes_around_arc(Q,  Tx_s, Tx_e, Dx_s, Dx_e, X_s, X_e, Angle_n_s, Angle_n_e,
         true ->
                 case Q of
                     0 ->
-                        {Ret_l,Left_end} =
+                        {Ret_L,Left_end} =
                             if
                                 Tx_e == Dx_e -> {[],Tx_e};
                                 true ->{[{Tx_e, yp_steppoint_on_arc(Tx_e + 1, Radius), yp_endpoint(Angle_n_e,Radius)}],Tx_e+1}
                             end,
-                        {Ret_r,Right_end} =
+                        {Ret_R,Right_end} =
                             if
                                 Tx_s == Dx_s -> {[],Tx_s};
         		        true -> {[{Tx_s, yp_endpoint(Angle_n_s,Radius), yp_steppoint_on_arc(Tx_s, Radius)}],Tx_s}
                             end;
 		    1 ->
-                        {Ret_l,Left_end} =
+                        {Ret_L,Left_end} =
                             if
                                 Tx_e == Dx_e -> {[],Tx_e};
                                 true ->{[{Tx_e - 1, yp_endpoint(Angle_n_e,Radius), yp_steppoint_on_arc(Tx_e, Radius)}],Tx_e}
                             end,
-                        {Ret_r,Right_end} =
+                        {Ret_R,Right_end} =
                             if
                                 Tx_s == Dx_s -> {[],Tx_s};
         		        true -> {[{Tx_s - 1, yp_steppoint_on_arc(Tx_s-1, Radius), yp_endpoint(Angle_n_s,Radius)}],Tx_s-1}
                             end;
 		    2 ->
-                        {Ret_l,Left_end} =
+                        {Ret_L,Left_end} =
                             if
                                 Tx_s == Dx_s -> {[],Tx_s};
                                 true ->{[{Tx_s - 1, yn_steppoint_on_arc(Tx_s, Radius), yn_endpoint(Angle_n_s,Radius)}],Tx_s}
                             end,
-                        {Ret_r, Right_end} =
+                        {Ret_R, Right_end} =
                             if
                                 Tx_e == Dx_e -> {[],Tx_e};
         		        true -> {[{Tx_e - 1, yn_steppoint_on_arc(Tx_e - 1, Radius), yn_endpoint(Angle_n_e,Radius)}],Tx_e-1}
                             end;
 		    3 ->
-                        {Ret_l,Left_end} =
+                        {Ret_L,Left_end} =
                             if
                                 Tx_s == Dx_s -> [];
                                 true ->[{Tx_s, yn_endpoint(Angle_n_s,Radius), yn_steppoint_on_arc(Tx_s+1, Radius)}]
                             end,
-                        {Ret_r, Right_end} =
+                        {Ret_R, Right_end} =
                             if
                                 Tx_e == Dx_e -> {[],Tx_e};
         		        true -> {[{Tx_e, yn_steppoint_on_arc(Tx_e, Radius), yn_endpoint(Angle_n_e,Radius)}],Tx_e}
                             end,
                 end,
 
-                Ret_m =
+                Ret_M =
                     if
                         Left_end == Right_end -> [];
 			true ->
                             Span = Right_end - Left_end,
                             Lst0 = lists:seq(Left_end,Right_end-1,1),
-                            Lst1 = lists:map(fun(Ox)->yp_steppoint_on_arc(Ox, Radius) end,lists:seq(Left_end,Right_end,1)),
-                            case Q of
-                                0 ->
-                                    Lst2 = lists:sublist(Lst1,1,Span),
-                                    Lst3 = lists:sublist(Lst1,2,Span),
-				1 ->[todo-xzm-gohere];
-				2 ->[todo-xzm-gohere];
-				3 ->[todo-xzm-gohere]
-                            end
+                            Lst = lists:map(fun(Ox)->yp_steppoint_on_arc(Ox, Radius) end,lists:seq(Left_end,Right_end,1)),
+                            Lst1 = lists:sublist(Lst,1,Span),
+                            Lst2 = lists:sublist(Lst,2,Span),
+                            Ret_M = 
+                                case Q of
+                                    0 -> lists:zip(Lst0,Lst2,Lst1);
+				    1 -> lists:zip(Lst0,Lst1,Lst2);
+				    2 -> lists:zip(Lst0,Lst2,Lst1);
+				    3 -> lists:zip(Lst0,Lst1,Lst2)
+                                end
                     end,
 
-                Ret_l ++ Ret_m ++ Ret_r
+                Ret_L ++ Ret_M ++ Ret_R
     end.
 
-%% Fx_s, Fx_e are the floors of Dx_s, Dx_e, i.e.,
-%%    Fx_s = if end,
-, Tx_s, Tx_e, Dx_s, Dx_e
 %% NOTICE: at the joining point, the stepbox for ray may not be indentical to that for arc.
-umerge_stepboxes_around_arc_ray(Q, Fx_s, Fx_e, Tx_s, Tx_e, Dx_s, Dx_e, X_s, X_e, Angle_n_s, Angle_n_e, Radius)->
-    [].
+umerge_stepboxes_around_arc_ray(Q, TL_stepboxes_ray,TL_stepboxes_arc)->
+    Length_ray = erlang:length(TL_stepboxes_ray),
+    Length_arc = erlang:length(TL_stepboxes_arc),
+    case Q of
+        0 ->
+            Ret_L = lists:sublist(TL_stepboxes_ray,1,Length_ray-1),
+            [{Ox,Y_L_ray,Y_U_ray}] = lists:sublist(TL_stepboxes_ray,Length_ray,1),
+            [{Ox,Y_L_arc,Y_U_arc}|Ret_R] = TL_stepboxes_arc;
+	1 ->
+            Ret_L = lists:sublist(TL_stepboxes_arc,1,Length_arc-1),
+            [{Ox,Y_L_arc,Y_U_arc}] = lists:sublist(TL_stepboxes_arc,Length_arc,1),
+            [{Ox,Y_L_ray,Y_U_ray}|Ret_R] = TL_stepboxes_ray;
+	2->
+            Ret_L = lists:sublist(TL_stepboxes_arc,1,Length_arc-1),
+            [{Ox,Y_L_arc,Y_U_arc}] = lists:sublist(TL_stepboxes_arc,Length_arc,1),
+            [{Ox,Y_L_ray,Y_U_ray}|Ret_R] = TL_stepboxes_ray;
+	3 ->
+            Ret_L = lists:sublist(TL_stepboxes_ray,1,Length_ray-1),
+            [{Ox,Y_L_ray,Y_U_ray}] = lists:sublist(TL_stepboxes_ray,Length_ray,1),
+            [{Ox,Y_L_arc,Y_U_arc}|Ret_R] = TL_stepboxes_arc
+    end,
+    Ret_M = [{Ox,erlang:min(Y_L_ray,Y_L_arc),erlang:max(Y_U_ray,Y_U_arc)}],
+    Ret_L ++ Ret_M ++ Ret_R.
 
-%%    ================ grid points list ================
+%% converge the cell reprentative LBs bounded by the lower and upper curves between two vertical lines.
+zip_stepboxes({Stepboxes_curve_L,Stepboxes_curve_U}) ->
+     lists:append(
+        lists:zipwith(
+            fun({Ox,Y_LL,Y_LU},{Ox,Y_UL,Y_UU})->lists:map(fun(Y)->{Ox,Y} end,lists:seq(erlang:min(Y_LL,Y_UL),erlang:max(Y_LU,Y_UU),1))end, 
+            Stepboxes_curve_L,
+            Stepboxes_curve_U)).
 
-%% converge the cell reprentative points in the arc in a quadrant
-%% zip_pointlist(Point_list_lower, Point_list_upper) ->
-%%
-%%     io:format("test - Point_list_lower = ~p~n",[Point_list_lower]),
-%%     io:format("test - Point_list_upper = ~p~n",[Point_list_upper]),
-%%
-%%     lists:append(lists:zipwith(fun({Ox,Y_l},{Ox,Y_u})->lists:map(fun(Y)->{Ox,Y} end,lists:seq(Y_l,Y_u,1))end, Point_list_lower, Point_list_upper)).
+%%    ================ output aoe sector grid(the LBs list) ================
 
+%%    Angle_amplitude =< ?two_pi.
+%%    Angle_s, Angle_amplitude are in radian.
+%%    Radius is positive.
+%%    Notice: every LB is representated with the coordinates, {X_p, Y_p},
+%%        where both X_p and Y_p are in the unit of pixels.
+aoe_grid_sector(Angle_s, Angle_amplitude, Radius)->
+    Two_pi = 2*math:pi(),
 
+    Angle_n_s =
+        if
+            Angle_amplitude == Two_pi -> 0;
+	    true -> angle_normal(Angle_s)
+        end,
 
+    Angle_e = Angle_n_s + Angle_amplitude,
+    Q_s = angle_quadrant(Angle_n_s),
 
-%% Q = 0,1,2,3 is the quadrant ordinal of Angle_n_s.
-%% Angle_n_s, Angle_n_e are in the same quadrant.
-%% Angle_n_s < Angle_n_e.
-%% Ox_n_s, Ox_n_e are the X cell ordinal reals for Angle_n_s < Angle_n_e respectively, i.e.,
-%%    Ox_n_s = Radius * math:cos(Angle_s)/?cell_side,
-%%    Ox_n_e = Radius * math:cos(Angle_e)/?cell_side,
-%%
-%%
-%% points_in_region_quadrant(Q, Ox_n_s, Ox_n_e, Angle_n_s, Angle_n_e, Radius)->
-%%     Point_list_ray_s = points_on_ray(Q, Ox_n_s, Angle_n_s,Radius),
-%%     Point_list_ray_e = points_on_ray(Q, Ox_n_e, Angle_n_e,Radius),
-%%     Point_list_arc = points_on_arc(Q, Ox_n_s, Ox_n_e, Radius),
-%%
-%%     io:format("test - Point_list_ray_s = ~p~n",[Point_list_ray_s]),
-%%     io:format("test - Point_list_ray_e = ~p~n",[Point_list_ray_e]),
-%%     io:format("test - Point_list_arc = ~p~n",[Point_list_arc]),
-%%
-%%     case Q of
-%%         0 ->
-%%             zip_pointlist(Point_list_ray_s,lists:umerge(Point_list_arc,Point_list_ray_e));
-%%         1 ->
-%%             zip_pointlist(Point_list_ray_e,lists:umerge(Point_list_arc,Point_list_ray_s));
-%%         2 ->
-%%             zip_pointlist(lists:umerge(Point_list_arc,Point_list_ray_e),Point_list_ray_s);
-%%         3 ->
-%%             zip_pointlist(lists:umerge(Point_list_arc,Point_list_ray_s),Point_list_ray_e)
-%%     end.
+    lists:map(
+        fun({O_x,O_y}) -> {O_x * ?cell_side_size, O_y * ?cell_side_size} end,
+        (lists:append(
+            lists:map(
+                fun({Q,A_n_s,A_n_e,Radius_}) ->
+                    X_s = Radius * math:cos(A_n_s),
+                    Dx_s = X_s/?cell_side_size,
+                    Tx_s = erlang:trunc(Dx_s),
+                    X_e = Radius * math:cos(A_n_e),
+                    Dx_e = X_e/?cell_side_size,
+                    Tx_e = erlang:trunc(Dx_e),
+                    Stepboxes_ray_s = stepboxes_around_ray(Q, Tx_s, Dx_s, X_s, A_n_s, Radius),
+                    Stepboxes_ray_e = stepboxes_around_ray(Q, Tx_e, Dx_e, X_e, A_n_e,Radius),
+                    Stepboxes_arc = stepboxes_around_arc(Q,  Tx_s, Tx_e, Dx_s, Dx_e, X_s, X_e, A_n_s, A_n_e, Radius),
+                    {Stepboxes_curve_L,Stepboxes_curve_U} =
+                        case Q of
+                            0 -> {Stepboxes_ray_s,umerge_stepboxes_around_arc_ray(0, TL_stepboxes_ray_e,TL_stepboxes_arc)};
+                            1 -> {Stepboxes_ray_e,umerge_stepboxes_around_arc_ray(0, TL_stepboxes_ray_s,TL_stepboxes_arc)};
+                            2 -> {umerge_stepboxes_around_arc_ray(0, TL_stepboxes_ray_e,TL_stepboxes_arc),Stepboxes_ray_s};
+                            3 -> {umerge_stepboxes_around_arc_ray(0, TL_stepboxes_ray_s,TL_stepboxes_arc),Stepboxes_ray_e}
+                        end,
+                    zip_stepboxes({Stepboxes_curve_L,Stepboxes_curve_U})
+                end,
+                sector_quadrant({Q_s,Angle_n_s,Angle_e,Radius})))).
 
-%%     Angle_amplitude =< ?two_pi.
-%%
-%% cell_LBs_sector(Angle_s, Angle_amplitude, Radius)->
-%%     Two_pi = 2*math:pi(),
-%%     Angle_n_s =
-%%         if
-%%             Angle_amplitude == Two_pi -> 0;
-%% 	    true -> angle_normal(Angle_s)
-%%         end,
-%%     Angle_e = Angle_n_s + Angle_amplitude,
-%%     Q = angle_quadrant(Angle_n_s),
-%%     lists:append(
-%%         lists:map(
-%%             fun({Q_,A_n_s,A_n_e,Radius_}) ->
-%%                 Ox_n_s = Radius_ * math:cos(A_n_s)/?cell_side,
-%%                 Ox_n_e = Radius_ * math:cos(A_n_e)/?cell_side,
-%%                 points_in_region_quadrant(Q_, Ox_n_s, Ox_n_e, A_n_s, A_n_e, Radius_)
-%%             end,
-%%             sector_quadrant({Q,Angle_n_s,Angle_e,Radius}))).
+%% ====================Deteminiation funtions==================
 
-%% ================================================
+%% determin whether the point({P_x,P_y}) is in the sector({Center_x, Center_y, Angle_s, Angle_amplitude, Radius}).
+point_in_sector_aoe(P_x,P_y,Center_x, Center_y, Angle_s, Angle_amplitude, Radius)->
+    lists:member(grid_point({P_x - Center_x, P_y - Center_y}),aoe_grid_sector(Angle_s, Angle_amplitude, Radius)).
 
-stepboxes_around_ray_test()->
-    ok.
-
-stepboxes_around_arc()->
-    ok.
-
-umerge_stepboxes_around_arc_ray_test()->
-    ok.
-
-cell_LBs_sector_test()->
-    %% Radius = 400,
-    %% AA = math:pi()/6,   %% AA, Angle_amplitude
-%%
-    %% As1 = 0,
-    %% TL1 = cell_LBs_sector(As1, AA, Radius), %% TL, Tuple List of points
-    %% io:format("TL1 = ~p~n",[TL1]),
-%%
-%%    As2 = math:pi()/3,    %% As, Angle_s
-%%    TL2 = cell_LBs_sector(As1, AA, Radius),
-%%    io:format("TL2 = ~p~n",[TL2]),
-
-    ok.
+%% {P_x, P_y} is the coordinates of a Point.
+%% {Center_x, Center_y} is the center of the sector.
+%% Aoe_grid is a tuple list of point coordinates.
+point_in_sector_aoe(P_x, P_y, Center_x, Center_y, Aoe_grid)->
+    lists:member(grid_point({P_x - Center_x, P_y - Center_y}), Aoe_grid).
 
 %% ====================TEST PART==================
 
+stepboxes_around_ray_test()->
+    io:format("test - ~n"),
+    ok.
+
+stepboxes_around_arc_test()->
+    io:format("test - ~n"),
+    ok.
+
+umerge_stepboxes_around_arc_ray_test()->
+    io:format("test - ~n"),
+    ok.
+
+aoe_grid_sector_test()->
+    io:format("test - ~n"),
+    ok.
+
 test() ->
+
+    Radius = 400,
+    AA = math:pi()/6,   %% AA, Angle_amplitude
+    As1 = 0,
+    TL1 = aoe_grid_sector(As1, AA, Radius), %% TL, Tuple List of points
+    io:format("TL1 = ~p~n",[TL1]),
+    %% As2 = math:pi()/3,    %% As, Angle_s
+    %% TL2 = aoe_grid_sector(As1, AA, Radius),
+    %% io:format("TL2 = ~p~n",[TL2]),
+
     stepboxes_around_ray_test(),
     stepboxes_around_arc(),
     umerge_stepboxes_around_arc_ray_test(),
-    cell_LBs_sector_test(),
+    aoe_grid_sector_test(),
 
     ok.
+%% ================================================
 
 start() ->
     test().
